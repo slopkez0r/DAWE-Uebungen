b177cd8f47c51b0eeea6279e3d81f8561d54d025
Maksim <maxim.reva.lol1@gmail.com>
2026-04-26 22:41:17 +0200

    first version of exercise 4 implemented

---
+++ server/simpleServer.js
@@ -0,0 +1,51 @@
+const express = require("express");
+var app = express();
+
+//a
+function convertCelciusFarenheit(celsius){
+    return (celsius*9/5)+32;
+}
+
+//b
+function getMin(collection){
+    return Array.getMin(collection);
+}
+
+function getMax(collection){
+    return Array.getMax(collection);
+}
+
+//c
+function getNrandomNumbers(n){
+    var arr = [];
+    
+    for(var i = 0; i<n; n++){
+        arr.push(Math.random());
+    }
+
+    return arr;
+}
+
+//d
+function reverse(string){
+    return string.reverse();
+}
+
+//e?
+
+//endpoint
+
+app.post('/:functionType/:value', async(req, res) => {
+    
+    const functionType = req.params.functionType;
+    const value = req.params.value;
+
+    switch(functionType){
+        case "convertCelsiusFarenheit": res.status(200).json(convertCelciusFarenheit(Number.parseFloat(value)));
+        case "getMin": res.status(200).json(getMin(value));
+        case "getMax": res.status(200).json(getMax(value));
+        case "getNrandomNumbers": res.status(200).json(getNrandomNumbers(Number.parseInt(value)));
+        case "reverse": res.status(200).json(everse(String(value)));
+    }
+});
+
